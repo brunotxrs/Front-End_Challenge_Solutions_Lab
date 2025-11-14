1178f1b1baab3e6e6dc481f3d834e1218ad5f944
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-11-14 18:18:22 -0400

    feat(js/navigation): Adiciona função de retorno para páginas internas da trilha Implementa a função 'returnToPageLogic()' em navigation-utils.js para gerenciar a navegação de retorno das páginas de solução. - Utiliza o novo ID 'back-page-logic' e o caminho relativo correto para voltar à página principal da trilha de Lógica de Programação. - Mantém a mesma lógica de animação de preenchimento (filled-left) e atraso de 1000ms. - Exporta a nova função para uso no app.js.

--- src/js/navigation-utils.js
+++ src/js/navigation-utils.js
@@ -28,4 +28,36 @@ function returnToIndex() {
     }
 }
 
-export { returnToIndex };
+function returnToPageLogic(){
+
+    const myBtnToBack = document.getElementById('back-page-logic');
+    const indexHtml = '../../01_Logica_de_Programacao/01_Logica_de_Programacao.html'
+
+    const REDIRECT_DELAY = 1000;
+
+    if (myBtnToBack) {
+
+        myBtnToBack.addEventListener('click', function(event){
+
+            event.preventDefault();
+
+            
+            if(myBtnToBack.classList.contains('filled-left')){
+                return;
+            }
+
+            myBtnToBack.classList.add('filled-left');
+
+
+            setTimeout(() => {
+
+                window.location.href = indexHtml; 
+
+            }, REDIRECT_DELAY);
+
+        });
+    }
+
+}
+
+export { returnToIndex, returnToPageLogic };
